a4d35b6572a8fbcd5b873f171b68b8a0c0aad2af
Miles Lucas <mdlucas@hawaii.edu>
2021-02-09 11:19:50 -0600

    clean up file

--- src/limbdark/limbdark.jl
+++ src/limbdark/limbdark.jl
@@ -1,29 +1,7 @@
 
 abstract type AbstractLimbDark end
 
-# struct LimbDarkLightCurve{CT<:AbstractVector,DT}
-#     u::CT
-#     c::CT
-#     c_norm::CT
-#     driver::DT
-# end
-
-# @doc raw"""
-#     LimbDarkLightCurve(u)
-
-# Limb-darkened light curve with limb-darkening coefficients `u`. The limb darkening law can be specThe limb-darkening profile has the quadratic form
-
-# ```math
-# \frac{I(\mu)}{I(1)}=1-u_1(1-\mu)-u_2(1-\mu)^2
-# ```
-# """
-# function LimbDarkLightCurve(u::AbstractVector{T}) where T
-#     u_ext = pushfirst!(u, -one(T))
-#     c = get_cl(u_ext)
-#     c_norm = c ./ (π * (c[1] + 2 * c[2] / 3))
-#     ld = GreensLimbDark(length(c) - 1)
-#     return LimbDarkLightCurve(u, c, c_norm, ld)
-# end
+Base.broadcastable(law::AbstractLimbDark) = Ref(law)
 
 (ld::AbstractLimbDark)(args...; kwargs...) = compute(ld, args...; kwargs...)
 
@@ -33,8 +11,6 @@ function compute(ld::AbstractLimbDark, orbit::AbstractOrbit, t, r)
     b = z / orbit.r_star
     return compute(ld, b, r)
 end
-
-export PolynomialLimbDark, IntegratedLimbDark
 
 include("elliptic.jl")
 include("poly.jl")
