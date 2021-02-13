849c3db6eac92a38273b2f25ce3a0f9460578bd9
Miles Lucas <mdlucas@hawaii.edu>
2021-02-12 23:33:33 -0600

    restructure

--- src/Transits.jl
+++ src/Transits.jl
@@ -14,6 +14,25 @@ include("orbits/Orbits.jl")
 @reexport using .Orbits
 using .Orbits: AbstractOrbit
 
-include("limbdark/limbdark.jl")
+
+abstract type AbstractLimbDark end
+
+Base.broadcastable(law::AbstractLimbDark) = Ref(law)
+
+(ld::AbstractLimbDark)(args...; kwargs...) = compute(ld, args...; kwargs...)
+
+function compute(ld::AbstractLimbDark, orbit::AbstractOrbit, t, r)
+    coords = Orbits.relative_position(orbit, t)
+    z = sqrt(coords[1]^2 + coords[2]^2)
+    b = z / orbit.r_star
+    return compute(ld, b, r)
+end
+
+include("elliptic.jl")
+include("poly.jl")
+include("quad.jl")
+include("integrated.jl")
+include("secondary.jl")
+
 
 end
