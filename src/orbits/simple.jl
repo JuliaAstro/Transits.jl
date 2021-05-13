7bb874f38885c5efdd496ade135ae8ae5770b4e4
icweaver <iweaver@cfa.harvard.edu>
2021-05-13 19:47:52 -0400

    dropped r_star

--- src/orbits/simple.jl
+++ src/orbits/simple.jl
@@ -6,11 +6,10 @@ struct SimpleOrbit{T,V,S} <: AbstractOrbit
     speed::S
     half_period::T
     ref_time::T
-    r_star::V
 end
 
 """
-    SimpleOrbit(; period, duration, t0=0, b=0.0, r_star=1.0)
+    SimpleOrbit(; period, duration, t0=0, b=0.0)
 
 Circular orbit parameterized by the basic observables of a transiting system.
 
@@ -19,18 +18,17 @@ Circular orbit parameterized by the basic observables of a transiting system.
 * `duration` - The duration of the transit, similar units as `period`.
 * `t0` - The midpoint time of the reference transit, similar units as `period`
 * `b` - The impact parameter of the orbit, unitless
-* `r_star` - The radius of the star, defaults to 1 solar radius
 """
-function SimpleOrbit(;period, duration, t0=zero(period), b=0.0, r_star=1.0)
+function SimpleOrbit(;period, duration, t0=zero(period), b=0.0)
     half_period = 0.5 * period
     duration > half_period && error("duration cannot be longer than half the period")
     speed = 2.0 * sqrt(1.0 - b^2.0) / duration
     ref_time =  t0 - half_period
     # normalize time types
     period, t0, duration, half_period, ref_time = promote(
-    period, t0, duration, half_period, ref_time, r_star
+    period, t0, duration, half_period, ref_time
     )
-    SimpleOrbit(period, t0, b, duration, speed, half_period, ref_time, r_star)
+    SimpleOrbit(period, t0, b, duration, speed, half_period, ref_time)
 end
 
 
@@ -42,7 +40,7 @@ relative_time(orbit::SimpleOrbit, t) = mod(t - orbit.ref_time, orbit.period) - o
 function relative_position(orbit::SimpleOrbit, t)
     Δt = relative_time(orbit, t)
     x = orbit.speed * Δt
-    y = orbit.b * orbit.r_star
+    y = orbit.b
     z = abs(Δt) < 0.5 * orbit.duration ? one(x) : -one(x)
     return SA[x, y, z]
 end
@@ -55,7 +53,7 @@ function in_transit(orbit::SimpleOrbit, t)
 end
 function in_transit(orbit::SimpleOrbit, t, r)
     Δt = relative_time.(orbit, t)
-    ϵ = √((orbit.r_star + r)^2.0 - orbit.b^2.0) / orbit.speed
+    ϵ = √((1.0 + r)^2.0 - orbit.b^2.0) / orbit.speed
     return findall(x -> abs(x) < ϵ, Δt)
 end
 
@@ -65,7 +63,7 @@ function flip(orbit::SimpleOrbit, ror)
     speed = orbit.speed / ror
     ref_time = orbit.t0
     return SimpleOrbit(
-        orbit.period, t0, b, orbit.duration, speed, orbit.half_period, ref_time, orbit.r_star
+        orbit.period, t0, b, orbit.duration, speed, orbit.half_period, ref_time
     )
 end
 
@@ -74,9 +72,8 @@ function Base.show(io::IO, orbit::SimpleOrbit)
     P = orbit.period
     b = orbit.b
     t0 = orbit.t0
-    r_star = orbit.r_star
     print(io,
-        "SimpleOrbit(P=$P, T=$T, t0=$t0, b=$b, r_star=$r_star)"
+        "SimpleOrbit(P=$P, T=$T, t0=$t0, b=$b)"
     )
 end
 
@@ -85,12 +82,10 @@ function Base.show(io::IO, ::MIME"text/plain", orbit::SimpleOrbit)
     P = orbit.period
     b = orbit.b
     t0 = orbit.t0
-    r_star = orbit.r_star
     print(io,
         "SimpleOrbit\n period: ", P,
         "\n duration: ", T,
         "\n t0: ", t0,
-        "\n b: ", b,
-        "\n r_star: ", r_star,
+        "\n b: ", b
     )
 end
--- test/orbits/simple.jl
+++ test/orbits/simple.jl
@@ -5,20 +5,19 @@ using Transits.Orbits: relative_position, in_transit
     t0 = 1.45
     b = 0.5
     duration = 0.12
-    r_star = 1.345
     t = t0 .+ range(-2.0*period, 2.0*period, length=5000)
     m0 = @. abs(mod(t - t0 + 0.5*period, period) - 0.5*period) < 0.5 * duration
 
-    orbit = SimpleOrbit(;period, t0, b, duration, r_star)
+    orbit = SimpleOrbit(;period, t0, b, duration)
     pos = @. relative_position(orbit, t)
     bs = map(v -> sqrt(v[1]^2.0 + v[2]^2.0), pos)
     zs = map(v->v[3], pos)
-    m = @. (bs ≤ r_star) & (zs > 0.0)
+    m = @. (bs ≤ 1.0) & (zs > 0.0)
     @test orbit.ref_time == -0.278
     @test m ≈ m0
 
     idxs = in_transit(orbit, t)
-    @test all(bs[idxs] .≤ r_star)
+    @test all(bs[idxs] .≤ 1.0)
     @test all(zs[idxs] .> 0.0)
 
     # TODO: Do we want this?
@@ -33,11 +32,10 @@ end
     t0 = 1.45
     b = 0.5
     duration = 0.12
-    r_star = 1.345
 
     t = t0 .+ range(-2.0*period, 2.0*period, length=5000)
     m0 = @. abs(mod(t - t0 + 0.5*period, period) - 0.5*period) < 0.5 * duration
-    orbit = SimpleOrbit(;period, t0, b, duration, r_star)
+    orbit = SimpleOrbit(;period, t0, b, duration)
     ld = PolynomialLimbDark([0.2, 0.3])
 
     lc1 = @. ld.(orbit, t, 0.01)
