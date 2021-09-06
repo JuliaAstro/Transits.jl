8b3cd36147b5b817fadff32ed7dcbf64fea6dc58
Ian Weaver <iweaver@cfa.harvard.edu>
2021-09-06 12:21:03 -0400

    Updating symbol conventions
    Co-authored-by: Miles Lucas <mdlucas@hawaii.edu>

--- src/orbits/keplerian.jl
+++ src/orbits/keplerian.jl
@@ -16,26 +16,25 @@ Keplerian orbit parameterized by the basic observables of a transiting 2-body sy
 
 # Parameters
 * `period`/`P` -- The orbital period of the planet [d].
-* `t_0`/`t₀` -- The midpoint time of the reference transit [d].
-* `t_p`/`tₚ` -- The time of periastron [d].
-* `duration`/`τ` -- The transit duration [d].
+* `t_0`/`t0` -- The midpoint time of the reference transit [d].
+* `t_p`/`tp` -- The time of periastron [d].
+* `duration`/`τ`/`T` -- The transit duration [d].
 * `a` -- The semi-major axis [R⊙].
-* `R_planet`/`Rₚ` -- The radius of the planet [R⊙].
-* `R_star`/`Rₛ` -- The radius of the star [R⊙].
-* `rho_star`/`ρₛ` -- The spherical star density [M⊙/R⊙³].
-* `RpRs` -- The ratio of the planet radius to star radius.
-* `aR_star`/`aRₛ` -- The ratio of the semi-major axis to star radius.
+* `aR_star`/`aRs` -- The ratio of the semi-major axis to star radius.
+* `R_planet`/`Rp` -- The radius of the planet [R⊙].
+* `R_star`/`Rs` -- The radius of the star [R⊙].
+* `rho_star`/`ρ_star` -- The spherical star density [M⊙/R⊙³].
+* `RpRs`/`r` -- The ratio of the planet radius to star radius.
 * `b` -- The impact parameter, bounded between 0 ≤ b ≤ 1.
-* `ecc` -- The eccentricity of the closed orbit, bounded between 0 ≤ ecc < 1.
+* `ecc`/`e` -- The eccentricity of the closed orbit, bounded between 0 ≤ ecc < 1.
 * `cos_omega`/`cos_ω` -- The cosine of the argument of periapsis.
 * `sin_omega`/`sin_ω` -- The sine of the argument of periapsis.
 * `incl` -- The inclination of the orbital plane relative to the axis perpendicular to the
            reference plane [rad]
 * `omega`/`ω` -- The argument of periapsis [rad].
 * `Omega`/`Ω` -- The longitude of the ascending node [rad].
-* `M_planet`/`Mₚ` -- The mass of the planet [M⊙].
-* `M_star`/`Mₛ` -- The mass of the star [M⊙].
-
+* `M_planet`/`Mp` -- The mass of the planet [M⊙].
+* `M_star`/`Ms` -- The mass of the star [M⊙].
 # Valid combinations
 The following flowchart can be used to determine which parameters can define a `KeplerianOrbit`:
 1. The `period` or `a` must be given. If both given, then neither `M_star` or `rho_star` can be defined because the stellar density is now implied.
