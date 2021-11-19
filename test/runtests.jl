2c9c972193c3a8cc8c8f9be0d42740fca1b30b2f
Ian Weaver <iweaver@cfa.harvard.edu>
2021-11-18 19:39:20 -0500

    test cleanup (mapreduce)
    Co-authored-by: Miles Lucas <mdlucas@hawaii.edu>

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ Unitful.preferunits(u"Rsun,Msun,d"...)
 
 # Numpy version of `isapprox`
 # https://stackoverflow.com/questions/27098844/allclose-how-to-check-if-two-arrays-are-close-in-julia/27100515#27100515
-allclose(a, b; rtol=1e-5, atol=1e-8) = all(@. abs(a - b) ≤ (atol + rtol*abs(b)))
+allclose(a, b; rtol=1e-5, atol=1e-8) = mapreduce((a,b) -> abs(a - b) ≤ (atol + rtol*abs(b)), &, a, b)
 
 const PLOT = get(ENV, "TEST_PLOTS", "false") == "true"
 PLOT && include("plots.jl")
