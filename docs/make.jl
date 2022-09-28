ec38a8181b68b20a8eb1b06254b9a0fbff0058c5
Miles Lucas <mdlucas@hawaii.edu>
2022-09-28 13:19:08 -1000

    fix docstrings breaking from Orbits not being loaded

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,17 @@
-using Transits
 using Documenter
+using Orbits
+using Transits
 
 setup = quote
     using Transits
 end
 
 DocMeta.setdocmeta!(Transits, :DocTestSetup, setup; recursive=true)
+
 include("pages.jl")
+
 makedocs(;
-    modules=[Transits],
+    modules=[Transits, Orbits],
     authors="Miles Lucas <mdlucas@hawaii.edu> and contributors",
     repo="https://github.com/juliaastro/Transits.jl/blob/{commit}{path}#L{line}",
     sitename="Transits.jl",
